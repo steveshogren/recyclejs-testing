56ad40b3ededcc784fb703109617d96f158093a2
jack <steve.a.shogren@gmail.com>
2018-08-12 05:46:00 -0400

    More consolidating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Timer from './TimerConfig';
+import Timer from './Timer';
 
 class App extends Component {
   render() {
--- src/Intents.js
+++ src/Intents.js
@@ -1,7 +1,7 @@
 import Rx from 'rxjs'
 
 export default function Intents(){
-    let CountRequested = new Rx.Subject(); 
+    let CountRequested = new Rx.Subject();
     //clients usually want EITHER an observable or an observable, not both.
     return {
         observers : { countRequested : new Rx.Subscriber(CountRequested) },
