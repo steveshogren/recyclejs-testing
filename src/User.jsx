02b8414b90f855b1ecb218dd260c68603bd286e7
jack <steve.a.shogren@gmail.com>
2018-08-20 05:56:52 -0400

    Trying to break out a combine

--- src/User.jsx
+++ src/User.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import recycle from 'recycle';
 import { combineLatest } from 'rxjs/observable/combineLatest';
-import Rx from 'rxjs';
 import intents from './Intents';
 import BookService from './BookService';
 
 // should be a singleton created at the top of the application
 let timerConf = configureUser(BookService(intents.outputs));
 
+let overwrite = (state,x) => x;
+const combine1 = function(outputs, fn) {
+            return [
+                combineLatest(
+                    outputs,
+                    fn
+                ).reducer(overwrite)];
+}
+
 export function configureUser(bookService) {
-    let overwrite = (state,x) => x;
 
     return {
         update (sources) {
-            return [
-                combineLatest(
-                    bookService.Users,
-                    (us) => {return ({ users: us });}
-                ).reducer(overwrite)];
+            return combine1 ([bookService.Users],
+                              (us) => ({ Users: us }));
         },
 
         // produce a view given the current state
         view (props, state) {
-            const users = state.users.map((user) =>
+            const users = state.Users.map((user) =>
                 <div key={user.id}>{user.name}</div>
             );
             return (
